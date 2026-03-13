de8c6833b08ec9863be1ef0eec896b39514259cc
feras <ferasaeoob@gmail.com>
2026-03-13 15:15:53 +0200

    working on place page

--- app/(main)/places/[slug]/page.tsx
+++ app/(main)/places/[slug]/page.tsx
@@ -1,7 +1,9 @@
-import  connectDB  from "@/lib/mongodb";
+import connectDB from "@/lib/mongodb";
 import Place from "@/database/place.model";
-import {SlugSchema} from "@/schemas/place.schema";
+import { SlugSchema } from "@/schemas/place.schema";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowLeft, MapPin } from "lucide-react";
 
 interface PageProps {
     params: Promise<{
@@ -27,28 +29,81 @@ export default async function PlacePage({ params }: PageProps) {
     if (!place) {
         return <div>Place not found</div>;
     }
+    function capitalizeFirst(str: string) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    const categoryColors: Record<string, string> = {
+        nature: "bg-green-400 hover:bg-black/70 text-green-700",
+        restaurant: "bg-orange-900/80",
+        activity: "bg-blue-900/80 hover:bg-black-900/40",
+        hotel: "bg-indigo-900/80",
+        viewpoint: "bg-purple-900/80",
+    };
 
     return (
-        <div className="max-w-5xl mx-auto p-6">
-            <Image
-                src={place.image}
-                alt={place.title.en}
-                width={1200}
-                height={600}
-                className="rounded-xl object-cover"
-            />
+        <div className=" pt-20 flex flex-col w-dvw h-full items-center px-3">
+            <div className="flex h-20 w-full max-w-[1200px] items-center ">
 
-            <h1 className="text-3xl font-bold mt-6">
-                {place.title.en}
-            </h1>
+                <Link href="/places" className=" flex text-lg font-bold gap-3"><ArrowLeft className="text-lg font-bold mt-1" /> Back to Explore</Link>
+            </div>
+            <div className="w-full max-w-[1200px] justify-center items-center ">
+                <div className=" max-w-[1200px] md:h-[500px] h-[350px]  relative rounded-3xl overflow-hidden shadow-xl">
 
-            <p className="mt-3 text-gray-600">
-                {place.description.en}
-            </p>
+                    <Image
+                        src={place.image}
+                        alt={place.title.en}
 
-            <p className="mt-4">
-                📍 {place.location.name}
-            </p>
+
+
+                        className="object-cover "
+                        fill
+                    />
+                </div>
+                <div className="flex flex-col mt-10 w-fit  ">
+                    <div className={` text-sm font-bold px-1.5 shadow-xl w-fit mb-2  ${categoryColors[place.category]}  rounded-md`}>{capitalizeFirst(place.category)}</div>
+                    <p className="flex">
+                        <MapPin className="w-4 h-4 mt-1 mr-2" /> {place.location.name}
+                    </p>
+
+
+                </div>
+
+
+
+                <div className="flex border-box border-black border-2">
+
+
+                    <div className="w-fit  border-box ">
+
+                        <h1 className="text-3xl font-bold mt-6">
+                            {place.title.en}
+                        </h1>
+
+                        <div className="mt-5 flex flex-col gap-6 pr-10">
+                            {place.description.en.split('\n\n').map((paragraph: string, index: number) => (
+                                <p
+                                    key={index}
+                                    className="text-gray-600 text-xl leading-relaxed"
+                                >
+                                    {paragraph}
+                                </p>
+                            ))}
+                        </div>
+
+
+
+                    </div>
+                    <aside className="w-70 border-box border-black border-2 max-h-80 p-5">
+                        <div>
+                            <h1 className="text-xl font-bold">Details</h1>
+                            <p>Location: {place.location.name}</p>
+                            <p>Category: {place.category}</p>
+                            <p>Price: {place.price}</p>
+
+                        </div>
+                    </aside>
+                </div>
+            </div>
         </div>
     );
 }
